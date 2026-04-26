945ac36135a663178f0d5fbcf00df20d4469a8b6
binitp2 <binitp2@gmail.com>
2026-04-26 19:43:34 +0530

    set operator c11 UPDATED

--- set operators.sql
+++ set operators.sql
@@ -1,6 +1,13 @@
 -- combine all data from emp and customers  into one table 
 
 select FirstName, LastName from Sales.Employees
+union
+select FirstName, LastName from Sales.Customers
+
+-- combine all data from emp and customers  into one table with duplicates 
+-- union all is more powerfull than union because it doesnot check duplicates condition 
+
+select FirstName, LastName from Sales.Employees
 union all
 select FirstName, LastName from Sales.Customers
 
